41221de8c772a0cc3e29da4296fec2228de3fb0e
DJG <djimenez@infowork.es>
2017-04-25 18:24:15 +0200

    Initial commit

---
+++ app/src/main/java/com/example/djg/myapplication/ModelCategory.java
@@ -0,0 +1,25 @@
+package com.example.djg.myapplication;
+
+public class ModelCategory {
+
+	private String title;
+	private String url;
+	
+	public ModelCategory(String title, String url) {
+		super();
+		this.title = title;
+		this.url = url;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+}
